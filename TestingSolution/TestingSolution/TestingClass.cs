730b42ba47dcd5533870cf40d4747c0147dbbd1b
Khalid <khalid@scaleablesolutions.com>
2019-12-19 00:45:57 +0500

    New Solution that have been made till now

---
+++ TestingSolution/TestingSolution/TestingClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingSolution
+{
+    public class TestingClass
+    {
+        // changes that have made by me
+        //This is new Data Added into the Solution
+    }
+}
